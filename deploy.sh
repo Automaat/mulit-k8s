ad3391042f14a24353cf4780539cca5014359bff
Marcin Skalski <marcin.skalski@allegro.pl>
2020-05-22 15:52:16 +0200

    added deployment script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t automaat/multi-client:latest -t automaat/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t automaat/multi-worker:latest -t automaat/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t automaat/multi-server:latest -t automaat/multi-server:$SHA -f ./server/Dockerfile ./serer
+
+docker push automaat/multi-client:latest
+docker push automaat/multi-server:latest
+docker push automaat/multi-worker:latest
+docker push automaat/multi-client:$SHA
+docker push automaat/multi-server:$SHA
+docker push automaat/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=automaat/multi-server:$SHA
+kubectl set image deployments/client-deployment server=automaat/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=automaat/multi-worker:$SHA
